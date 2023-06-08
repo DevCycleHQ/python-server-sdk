6370a89192b8e56b55394d432ac01c4f7fedc849
chris-hoefgen <chris.hoefgen@taplytics.com>
2023-06-08 14:45:53 -0700

    bug fix - file not getting closed

--- devcycle_python_sdk/util/version.py
+++ devcycle_python_sdk/util/version.py
@@ -5,5 +5,5 @@ def sdk_version() -> str:
     """
     Returns the current version of this SDK as a semantic version string
     """
-    version_file = open(os.path.dirname(__file__) + '/../VERSION.txt')
-    return version_file.read().strip()
+    with open(os.path.dirname(__file__) + '/../VERSION.txt', 'r') as version_file:
+        return version_file.read().strip()
