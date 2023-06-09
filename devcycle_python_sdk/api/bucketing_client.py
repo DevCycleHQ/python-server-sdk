a0e4b97a73f07761f8b0152b915f5148185c2d2c
Rob ODwyer <rob.odwyer@taplytics.com>
2023-06-09 11:44:31 -0700

    add retries with exponential backoff

--- devcycle_python_sdk/api/bucketing_client.py
+++ devcycle_python_sdk/api/bucketing_client.py
@@ -1,10 +1,16 @@
+import logging
+import math
 from os.path import join
+import random
+import time
 from typing import Dict, List
 
 import requests
 
 from devcycle_python_sdk.dvc_options import DVCCloudOptions
 from devcycle_python_sdk.models import Variable, UserData, Feature, Event
+
+logger = logging.getLogger(__name__)
 
 
 class BucketingAPIClient:
@@ -22,10 +28,34 @@ class BucketingAPIClient:
     def _url(self, *path_args: str) -> str:
         return join(self.options.bucketing_API_URI, "v1", *path_args)
 
-    def variable(self, key: str, user: UserData) -> Variable:
-        res = self.session.post(self._url("variables", key), json=user.to_json())
-        res.raise_for_status()
+    def request(self, method: str, url: str, **kwargs) -> dict:
+        retries_remaining = self.options.request_retries
+        timeout = self.options.request_timeout
+
+        attempts = 1
+        while retries_remaining > 0:
+            try:
+                res = self.session.request(method, url, timeout=timeout, **kwargs)
+                res.raise_for_status()
+                break
+            except requests.exceptions.RequestException as e:
+                logger.error(
+                    f"DevCycle cloud bucketing request failed (attempt {attempts}): {e}"
+                )
+                retries_remaining -= 1
+                if retries_remaining:
+                    retry_delay = exponential_backoff(attempts)
+                    time.sleep(retry_delay)
+                    attempts += 1
+                    continue
+
+                raise DVCCloudClientException(e)
+
         data: dict = res.json()
+        return data
+
+    def variable(self, key: str, user: UserData) -> Variable:
+        data = self.request("POST", self._url("variables", key), json=user.to_json())
 
         return Variable(
             _id=str(data.get("_id")),
@@ -35,9 +65,7 @@ class BucketingAPIClient:
         )
 
     def variables(self, user: UserData) -> Dict[str, Variable]:
-        res = self.session.post(self._url("variables"), json=user.to_json())
-        res.raise_for_status()
-        data: dict = res.json()
+        data = self.request("POST", self._url("variables"), json=user.to_json())
 
         result: Dict[str, Variable] = {}
         for key, value in data.items():
@@ -51,9 +79,7 @@ class BucketingAPIClient:
         return result
 
     def features(self, user: UserData) -> Dict[str, Feature]:
-        res = self.session.post(self._url("features"), json=user.to_json())
-        res.raise_for_status()
-        data: dict = res.json()
+        data = self.request("POST", self._url("features"), json=user.to_json())
 
         result: Dict[str, Feature] = {}
         for key, value in data.items():
@@ -70,13 +96,27 @@ class BucketingAPIClient:
         return result
 
     def track(self, user: UserData, events: List[Event]) -> str:
-        res = self.session.post(
+        data = self.request(
+            "POST",
             self._url("track"),
             json={
                 "user": user.to_json(),
                 "events": [event.to_json() for event in events],
             },
         )
-        res.raise_for_status()
-        message = res.json().get("message", "")
+        message = data.get("message", "")
         return message
+
+
+class DVCCloudClientException(Exception):
+    def __init__(self, cause: Exception):
+        self.cause = cause
+
+
+def exponential_backoff(attempt: int) -> float:
+    """
+    Exponential backoff starting with 200ms +- 0...40ms jitter
+    """
+    delay = math.pow(2, attempt) * 0.1
+    random_sum = delay * 0.2 * random.random()
+    return delay + random_sum
--- devcycle_python_sdk/dvc_options.py
+++ devcycle_python_sdk/dvc_options.py
@@ -1,12 +1,16 @@
 class DVCCloudOptions:
-    def __init__(self,
-                 enable_edge_db: bool = False,
-                 events_api_uri: str = "https://events.devcycle.com/",
-                 config_cdn_uri: str = "https://config-cdn.devcycle.com/",
-                 bucketing_api_uri: str = "https://bucketing-api.devcycle.com/",
-                 request_timeout: int = 10000):
+    def __init__(
+        self,
+        enable_edge_db: bool = False,
+        events_api_uri: str = "https://events.devcycle.com/",
+        config_cdn_uri: str = "https://config-cdn.devcycle.com/",
+        bucketing_api_uri: str = "https://bucketing-api.devcycle.com/",
+        request_timeout: int = 5,  # seconds
+        request_retries: int = 5,
+    ):
         self.enable_edge_db = enable_edge_db
         self.events_API_URI = events_api_uri
         self.config_CDN_URI = config_cdn_uri
         self.bucketing_API_URI = bucketing_api_uri
         self.request_timeout = request_timeout
+        self.request_retries = request_retries
