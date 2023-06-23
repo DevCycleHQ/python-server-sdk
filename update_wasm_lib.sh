1574124677d9fdeff54cc2e1dcc95e9825701be7
chris-hoefgen <chris.hoefgen@taplytics.com>
2023-06-23 09:12:36 -0700

    new utility script to pull wasm lib releases based on version

---
+++ update_wasm_lib.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+BUCKETING_LIB_VERSION="1.7.0"
+
+if [[ -n "$1" ]]; then
+  BUCKETING_LIB_VERSION="$1"
+fi
+
+cd devcycle_python_sdk
+rm bucketing-lib.release.wasm
+echo "Downloading bucketing lib version $BUCKETING_LIB_VERSION"
+wget "https://unpkg.com/@devcycle/bucketing-assembly-script@$BUCKETING_LIB_VERSION/build/bucketing-lib.release.wasm"
