5d0cd34a4f51bb779ebedb41486851d0216aec8d
chris-hoefgen <chris.hoefgen@taplytics.com>
2023-07-05 14:49:50 -0700

    bump version to 3.0.0

--- test/util/test_version.py
+++ test/util/test_version.py
@@ -9,7 +9,7 @@ logger = logging.getLogger(__name__)
 
 class VersionTest(unittest.TestCase):
     def test_sdk_version(self):
-        expected_version = "2.0.1"
+        expected_version = "3.0.0"
         version = sdk_version()
         self.assertEqual(version, expected_version)
 
