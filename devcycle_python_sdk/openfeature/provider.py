447bce9c602da9dec4a20bc5a9813a5e7ab8681f
chris-hoefgen <chris.hoefgen@taplytics.com>
2023-10-18 07:41:51 -0700

    new provider and unit tests

---
+++ devcycle_python_sdk/openfeature/provider.py
@@ -0,0 +1,187 @@
+import typing
+import logging
+
+from devcycle_python_sdk import DevCycleLocalClient
+from devcycle_python_sdk.models.user import DevCycleUser
+
+from openfeature.provider.provider import AbstractProvider
+from openfeature.evaluation_context import EvaluationContext
+from openfeature.flag_evaluation import FlagResolutionDetails, Reason
+from openfeature.exception import ErrorCode
+from openfeature.hook import Hook
+from openfeature.provider.metadata import Metadata
+
+logger = logging.getLogger(__name__)
+
+
+class UserDataError(Exception):
+    pass
+
+
+def _set_custom_value(custom_data: dict, key: str, value: typing.Optional[typing.Any]):
+    if (
+        custom_data is not None
+        and key
+        and value is not None
+        and isinstance(value, (str, int, float, bool))
+    ):
+        custom_data[key] = value
+
+
+def _create_user_from_context(context: EvaluationContext) -> DevCycleUser:
+    user_id = None
+
+    if context:
+        if context.targeting_key:
+            user_id = context.targeting_key
+        elif context.attributes and "userId" in context.attributes.keys():
+            user_id = context.attributes["userId"]
+
+    if not user_id:
+        raise UserDataError(
+            "DevCycle: Evaluation context does not contain a targeting key or userId attribute"
+        )
+
+    user = DevCycleUser(user_id=user_id)
+    custom_data = {}
+    private_custom_data = {}
+    if context and context.attributes:
+        for key, value in context.attributes.items():
+            if value:
+                if key == "email" and isinstance(value, str):
+                    user.email = value
+                elif key == "name" and isinstance(value, str):
+                    user.name = value
+                elif key == "language" and isinstance(value, str):
+                    user.language = value
+                elif key == "country" and isinstance(value, str):
+                    user.country = value
+                elif key == "appVersion" and isinstance(value, str):
+                    user.appVersion = value
+                elif key == "appBuild" and isinstance(value, str):
+                    user.appBuild = value
+                elif key == "deviceModel" and isinstance(value, str):
+                    user.deviceModel = value
+                elif key == "customData" and isinstance(value, dict):
+                    for k, v in value.items():
+                        _set_custom_value(custom_data, k, v)
+                elif key == "privateCustomData" and isinstance(value, dict):
+                    for k, v in value.items():
+                        _set_custom_value(private_custom_data, k, v)
+
+    if custom_data:
+        user.customData = custom_data
+
+    if private_custom_data:
+        user.privateCustomData = private_custom_data
+
+    return user
+
+
+class DevCycleProvider(AbstractProvider):
+    def __init__(self, devcycle_client: DevCycleLocalClient):
+        self.client = devcycle_client
+        self.meta_data = Metadata(name="DevCycle Provider")
+
+    def get_metadata(self) -> Metadata:
+        return self.meta_data
+
+    def get_provider_hooks(self) -> typing.List[Hook]:
+        return []
+
+    def _resolve_details(
+        self,
+        flag_key: str,
+        default_value: typing.Any,
+        evaluation_context: typing.Optional[EvaluationContext] = None,
+    ) -> FlagResolutionDetails[typing.Any]:
+        if self.client.is_initialized():
+            try:
+                user: DevCycleUser = _create_user_from_context(evaluation_context)
+
+                variable = self.client.variable(
+                    key=flag_key, user=user, default_value=default_value
+                )
+
+                if variable is None:
+                    return FlagResolutionDetails(
+                        value=default_value,
+                        reason=Reason.DEFAULT,
+                    )
+                else:
+                    return FlagResolutionDetails(
+                        value=variable.value,
+                        reason=Reason.DEFAULT
+                        if variable.isDefaulted
+                        else Reason.TARGETING_MATCH,
+                    )
+            except ValueError as e:
+                return FlagResolutionDetails(
+                    value=default_value,
+                    reason=Reason.ERROR,
+                    error_code=ErrorCode.INVALID_CONTEXT,
+                    error_message=str(e),
+                )
+            except UserDataError as e:
+                return FlagResolutionDetails(
+                    value=default_value,
+                    reason=Reason.ERROR,
+                    error_code=ErrorCode.TARGETING_KEY_MISSING,
+                    error_message=str(e),
+                )
+            except Exception as e:
+                return FlagResolutionDetails(
+                    value=default_value,
+                    reason=Reason.ERROR,
+                    error_code=ErrorCode.PARSE_ERROR,
+                    error_message=str(e),
+                )
+        else:
+            logger.debug(
+                "DevCycle: variable evaluation called before client has initialized"
+            )
+            return FlagResolutionDetails(
+                value=default_value,
+                reason=Reason.ERROR,
+                error_code=ErrorCode.PROVIDER_NOT_READY,
+            )
+
+    def resolve_boolean_details(
+        self,
+        flag_key: str,
+        default_value: bool,
+        evaluation_context: typing.Optional[EvaluationContext] = None,
+    ) -> FlagResolutionDetails[bool]:
+        return self._resolve_details(flag_key, default_value, evaluation_context)
+
+    def resolve_string_details(
+        self,
+        flag_key: str,
+        default_value: str,
+        evaluation_context: typing.Optional[EvaluationContext] = None,
+    ) -> FlagResolutionDetails[str]:
+        return self._resolve_details(flag_key, default_value, evaluation_context)
+
+    def resolve_integer_details(
+        self,
+        flag_key: str,
+        default_value: int,
+        evaluation_context: typing.Optional[EvaluationContext] = None,
+    ) -> FlagResolutionDetails[int]:
+        return self._resolve_details(flag_key, default_value, evaluation_context)
+
+    def resolve_float_details(
+        self,
+        flag_key: str,
+        default_value: float,
+        evaluation_context: typing.Optional[EvaluationContext] = None,
+    ) -> FlagResolutionDetails[float]:
+        return self._resolve_details(flag_key, default_value, evaluation_context)
+
+    def resolve_object_details(
+        self,
+        flag_key: str,
+        default_value: typing.Union[dict, list],
+        evaluation_context: typing.Optional[EvaluationContext] = None,
+    ) -> FlagResolutionDetails[typing.Union[dict, list]]:
+        pass
---
+++ test/openfeature/test_provider.py
@@ -0,0 +1,201 @@
+import logging
+import unittest
+
+
+from openfeature.provider.provider import EvaluationContext
+from openfeature.flag_evaluation import FlagResolutionDetails, Reason
+from openfeature.exception import ErrorCode
+
+from devcycle_python_sdk.openfeature.provider import (
+    _set_custom_value,
+    _create_user_from_context,
+    UserDataError,
+    DevCycleProvider
+)
+
+logger = logging.getLogger(__name__)
+
+class DevCycleProviderTest(unittest.TestCase):
+    def test_resolve_details_client_not_ready(self):
+        client = unittest.MagicMock()
+        client.is_initialized.return_value = False
+        provider = DevCycleProvider(client)
+
+        details = provider._resolve_details("test-flag", False, EvaluationContext("test-user"))
+
+        self.assertIsNotNone(details)
+        self.assertEqual(details.value, False)
+        self.assertEqual(details.reason, Reason.ERROR)
+        self.assertEqual(details.error_code, ErrorCode.PROVIDER_NOT_READY)
+
+    def test_resolve_details_client_no_user_in_context(self):
+        client = unittest.MagicMock()
+        client.is_initialized.return_value = True
+        provider = DevCycleProvider(client)
+        context = EvaluationContext(targeting_key=None, attributes={})
+        details = provider._resolve_details("test-flag", False, context)
+
+        self.assertIsNotNone(details)
+        self.assertEqual(details.value, False)
+        self.assertEqual(details.reason, Reason.ERROR)
+        self.assertEqual(details.error_code, ErrorCode.TARGETING_KEY_MISSING)
+
+    def test_resolve_details_client_no_key(self):
+        client = unittest.MagicMock()
+        client.is_initialized.return_value = True
+        provider = DevCycleProvider(client)
+        context = EvaluationContext(targeting_key=None, attributes={})
+        details = provider._resolve_details(None, False, context)
+
+        self.assertIsNotNone(details)
+        self.assertEqual(details.value, False)
+        self.assertEqual(details.reason, Reason.ERROR)
+        self.assertEqual(details.error_code, ErrorCode.TARGETING_KEY_MISSING)
+
+
+
+class UserDataFromContextTest(unittest.TestCase):
+    def test_create_user_from_context_no_context(self):
+        with self.assertRaises(UserDataError):
+            _create_user_from_context(None)
+
+    def test_create_user_from_context_no_user_id(self):
+        with self.assertRaises(UserDataError):
+            _create_user_from_context(
+                EvaluationContext(targeting_key=None, attributes={})
+            )
+
+        with self.assertRaises(UserDataError):
+            _create_user_from_context(
+                EvaluationContext(targeting_key=None, attributes=None)
+            )
+
+        with self.assertRaises(UserDataError):
+            _create_user_from_context(
+                EvaluationContext(targeting_key=None, attributes={"userId": None})
+            )
+
+    def test_create_user_from_context_only_user_id(self):
+        test_user_id = "12345"
+        context = EvaluationContext(targeting_key=test_user_id, attributes=None)
+        user = _create_user_from_context(context)
+        self.assertIsNotNone(user)
+        self.assertEqual(user.user_id, test_user_id)
+
+        context = EvaluationContext(
+            targeting_key=None, attributes={"userId": test_user_id}
+        )
+        user = _create_user_from_context(context)
+        self.assertIsNotNone(user)
+        self.assertEqual(user.user_id, test_user_id)
+
+        # ensure that targeting_key takes precedence over userId
+        context = EvaluationContext(
+            targeting_key=test_user_id, attributes={"userId": "99999"}
+        )
+        user = _create_user_from_context(context)
+        self.assertIsNotNone(user)
+        self.assertEqual(user.user_id, test_user_id)
+
+    def test_create_user_from_context_with_attributes(self):
+        test_user_id = "12345"
+        context = EvaluationContext(
+            targeting_key=test_user_id,
+            attributes={
+                "userId": "1234",
+                "email": "someone@example.com",
+                "name": "John Doe",
+                "language": "en",
+                "country": "US",
+                "appVersion": "1.0.0",
+                "appBuild": "1",
+                "deviceModel": "iPhone X21",
+            },
+        )
+        user = _create_user_from_context(context)
+        self.assertIsNotNone(user)
+        self.assertEqual(user.user_id, test_user_id)
+        self.assertEqual(user.email, context.attributes["email"])
+        self.assertEqual(user.name, context.attributes["name"])
+        self.assertEqual(user.language, context.attributes["language"])
+        self.assertEqual(user.country, context.attributes["country"])
+        self.assertEqual(user.appVersion, context.attributes["appVersion"])
+        self.assertEqual(user.appBuild, context.attributes["appBuild"])
+        self.assertEqual(user.deviceModel, context.attributes["deviceModel"])
+
+    def test_create_user_from_context_with_custom_data(self):
+        test_user_id = "12345"
+        context = EvaluationContext(
+            targeting_key=test_user_id,
+            attributes={
+                "customData": {
+                    "strValue": "hello",
+                    "intValue": 123,
+                    "floatValue": 3.1456,
+                    "boolValue": True,
+                },
+                "privateCustomData": {
+                    "strValue": "world",
+                    "intValue": 789,
+                    "floatValue": 0.0001,
+                    "BoolValue": False,
+                },
+            },
+        )
+        user = _create_user_from_context(context)
+        self.assertIsNotNone(user)
+        self.assertEqual(user.user_id, test_user_id)
+
+        self.assertEqual(user.customData, {
+            "strValue": "hello",
+            "intValue": 123,
+            "floatValue": 3.1456,
+            "boolValue": True,
+        })
+        self.assertEqual(user.privateCustomData, {
+            "strValue": "world",
+            "intValue": 789,
+            "floatValue": 0.0001,
+            "BoolValue": False,
+        })
+
+
+    def test_set_custom_value(self):
+        custom_data = {}
+        _set_custom_value(custom_data, None, None)
+        self.assertDictEqual(custom_data, {})
+
+        custom_data = {}
+        _set_custom_value(custom_data, "key1", None)
+        self.assertDictEqual(custom_data, {})
+
+        custom_data = {}
+        _set_custom_value(custom_data, "key1", "value1")
+        self.assertDictEqual(custom_data, {"key1": "value1"})
+
+        custom_data = {}
+        _set_custom_value(custom_data, "key1", 1)
+        self.assertDictEqual(custom_data, {"key1": 1})
+
+        custom_data = {}
+        _set_custom_value(custom_data, "key1", 3.1456)
+        self.assertDictEqual(custom_data, {"key1": 3.1456})
+
+        custom_data = {}
+        _set_custom_value(custom_data, "key1", True)
+        self.assertDictEqual(custom_data, {"key1": True})
+
+        custom_data = {}
+        _set_custom_value(custom_data, "map_data", {"hello": "world"})
+        self.assertDictEqual(custom_data, {})
+
+        custom_data = {}
+        _set_custom_value(custom_data, "list_data", ["one", "two", "three"])
+        self.assertDictEqual(custom_data, {})
+
+
+
+
+
+if __name__ == "__main__":
+    unittest.main()
